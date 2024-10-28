0fde9a5babaa08dbc8b322eb430c16f350bb5732
Nazmi05 <narikoscarlet@gmail.com>
2024-10-28 16:55:51 +0600

    Add files via upload

---
+++ hw-filter/main.js
@@ -0,0 +1,57 @@
+/*
+const task1=function(){
+const numbers = [1, 2, 3, 4, 5, 6];
+
+const filternumbers=numbers.filter(item=>{
+    return item%2===0;
+})
+console.log(filternumbers);
+}
+*/
+/*const task2=function(){
+const string1=['кот', 'собака', 'тигр', 'лев'];
+const Newstring=string1.filter(item=>{
+    return item.length>3
+})
+
+console.log(Newstring);
+}
+*/
+/*const task3=function(){
+const overten =  [5, 12, 8, 20, 3];
+const filterten=overten.filter(item=>{
+    return item>10;
+})
+
+console.log(filterten);
+}
+*/
+
+/*const task4=function(){
+const Astrings =  ['апельсин', 'яблоко', 'арбуз', 'банан'];
+
+const filterA=Astrings.filter(item=>{
+    return item[0]==="а";
+})
+
+console.log(filterA);
+}
+*/
+
+/*const task4=function(){
+console.log(filterbyage);
+const people = [
+      { name: "John", age: 17 },
+      { name: "Jane", age: 22 },
+      { name: "Mark", age: 19 },
+      { name: "Anna", age: 16 },
+      { name: "Bob", age: 24 }
+    ];
+
+const filterbyage=people.filter(item=>{
+    return item.age>=18;
+})
+
+console.log(filterbyage);
+}
+*/
